2063521ae3d63a82b12e4667ac8ac8f19ecaca62
brettzky10 <brett.linseman@gmail.com>
2023-11-06 15:12:20 -0500

    changed to parse

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -12,7 +12,7 @@ if (!admin.apps.length){
         credential: admin.credential.cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY,
+            privateKey: destPath,
         })
     });
 }
@@ -24,7 +24,7 @@ const adminDb = initFirestore({
     credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        privateKey: destPath,
     })
 })
 
