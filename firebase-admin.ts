1b54a45bae3912f6c3eb97d0cc5e6c03874c17b9
brettzky10 <brett.linseman@gmail.com>
2023-11-06 15:06:50 -0500

    changed parse

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -3,12 +3,14 @@ import admin from "firebase-admin";
 
 let app;
 
+const destPath = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/gm, "\n") ? process.env.FIREBASE_PRIVATE_KEY : "undefined"
+
 if (!admin.apps.length){
     app = admin.initializeApp({
         credential: admin.credential.cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/gm, "\n"),
         })
     });
 }
@@ -17,7 +19,7 @@ const adminDb = initFirestore({
     credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace("/\\n/gm", "\n"),
     })
 })
 
